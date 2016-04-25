2089c7dfd790c9669fb7c7e56b09ccce5414b8dd
Alfred Johansson <alfred.k.johansson@gmail.com>
2016-04-25 17:10:37 +0200

    Uppdatera så att projekt ID och Plan läggs i shared pref. Som projectSettings. Så plan och projekt kan nås från alla aktiviteter. Det går även att läsa från databasen vilka projekt som valts även vilket plan man vill arbeta på.

--- app/src/main/java/com/example/alfred/craftsman_part/ChooseFloorActivity.java
+++ app/src/main/java/com/example/alfred/craftsman_part/ChooseFloorActivity.java
@@ -2,6 +2,7 @@ package com.example.alfred.craftsman_part;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.view.View;
@@ -14,6 +15,7 @@ import android.widget.TextView;
 public class ChooseFloorActivity extends Activity {
 
     String floor;
+    DatabaseHelper db;
 
 
 
@@ -24,41 +26,42 @@ public class ChooseFloorActivity extends Activity {
 
         setContentView(R.layout.activity_choose_floor);
 
+        db = new DatabaseHelper(this);
+        SharedPreferences getID = getSharedPreferences("projectSettings",MODE_PRIVATE);
+        int i = getID.getInt("projectID", 0);
+
+
         final ListView floor_list = (ListView)findViewById(R.id.floor_list);
         final TextView floormap = (TextView)findViewById(R.id.floor_selected_txt);
 
-        String [] floors = {"Plan 1", "Plan 2", "Plan 3", "Plan 4", "Plan 5", "Plan 6", "Plan 7", "Plan 8", "Plan 9", "Plan 10", "Plan 11", "Plan 12"};
+        String [] floors = db.getFloors(i); //{"Plan 1", "Plan 2", "Plan 3", "Plan 4", "Plan 5", "Plan 6", "Plan 7", "Plan 8", "Plan 9", "Plan 10", "Plan 11", "Plan 12"};
 
         ListAdapter listAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, floors);
 
         floor_list.setAdapter(listAdapter);
 
-
         floor_list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
                 floor = floor_list.getItemAtPosition(position).toString();
                 floormap.setText(floor);
-
             }
         });
+
+
     }
 
 
     public void chooseFloor(View view) {
 
         Intent goBackToMain = new Intent();
-        goBackToMain.putExtra("floorID", floor);
-        goBackToMain.addFlags(1);
-
+        //goBackToMain.putExtra("floorID", floor);
+        SharedPreferences floorID = getSharedPreferences("projectSettings", MODE_PRIVATE);
+        floorID.edit().putInt("projectFloor", Integer.parseInt(floor)).commit();
         setResult(RESULT_OK, goBackToMain);
         finish();
 
     }
 
-    public void getFloorID(){
-
-    }
 }
